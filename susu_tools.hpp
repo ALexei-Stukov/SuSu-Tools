725f70143d6458a8fcc3ccae87852c8f0e6e35da
ZZZ <ZZZ>
2024-07-24 17:12:20 +0800

    add all .hpp file on this file,use namespace susu_tools.

---
+++ susu_tools.hpp
@@ -0,0 +1,15 @@
+//all tools on here
+#include "susu_"
+#include "susu_cache.hpp"
+#include "susu_"
+#include "susu_"
+#include "susu_"
+#include "susu_"
+#include "susu_"
+#include "susu_"
+#include "susu_"
+#include "susu_"
+#include "susu_"
+
+
+using namespace susu_tools;
