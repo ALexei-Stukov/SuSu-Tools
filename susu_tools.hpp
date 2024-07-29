57b2b238a5eb35ee824435499fa10f41fede6183
ZZZ <ZZZ>
2024-07-29 15:20:53 +0800

    collect all hpp in a file.

--- susu_tools.hpp
+++ susu_tools.hpp
@@ -1,15 +1,13 @@
-//all tools on here
-#include "susu_"
+//all .hpp on here
+
+#include "susu_epoll.hpp"
+#include "susu_initparam.hpp"
+#include "susu_socket.hpp"
+#include "susu_http.hpp"
 #include "susu_cache.hpp"
-#include "susu_"
-#include "susu_"
-#include "susu_"
-#include "susu_"
-#include "susu_"
-#include "susu_"
-#include "susu_"
-#include "susu_"
-#include "susu_"
+#include "susu_timer.hpp"
+#include "susu_task-queue.hpp"
+#include "susu_thread_worker.hpp"
+#include "susu_thread_pool.hpp"
 
-
-using namespace susu_tools;
+//don't use namespace susu_tools on hpp.
